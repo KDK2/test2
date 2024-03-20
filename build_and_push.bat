fda864358102ff223dfa23f9fb3c3c24ff6b21e7
KDK <kdk9678@gmail.com>
2024-03-20 21:10:10 +0900

    Successful build

--- build_and_push.bat
+++ build_and_push.bat
@@ -4,6 +4,8 @@ call mingw32-make.exe -j4 C:\KC\build-untitled2-mingw-Debug || goto :error
 
 :: 빌드가 성공적으로 완료되었으면 git 명령 실행
 git add .
+git update-index --assume-unchanged untitled2.pro.user
+git update-index --assume-unchanged untitled2.pro.user.6310840
 git commit -m "Successful build"
 git push
 goto :end
--- robot.cpp
+++ robot.cpp
@@ -23,7 +23,7 @@ Robot::Robot():
     ig.f_param.mparam.q_v=-1.0;
     ig.f_param.rparam.d_o=0.2;
     ig.f_param.rparam.d_oq=0.4;
-    ig.f_param.rparam.k_vo=1.0/(static_cast<double>(is.sparam.num_sensors)+85);
+    ig.f_param.rparam.k_vo=1.0/(static_cast<double>(is.sparam.num_sensors)+84);
     ig.f_param.rparam.q_o=-1.0;
     ig.f_param.qparam.k_vq=5.0*ig.f_param.rparam.k_vo;//d_o와 묶여있는 변수다.
     ig.f_param.qparam.q_q=-1.0;
