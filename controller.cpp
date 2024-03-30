142220e31a39661efc5c5084bbec07ccbc6d372e
KDK2 <kdk9678@gmail.com>
2024-03-30 19:24:58 +0900

    Successful build

--- controller.cpp
+++ controller.cpp
@@ -178,8 +178,8 @@ double Controller::cost(std::vector<Generator::path> path, std::vector<Generator
     double varianceX=0.0;
     double varianceY=0.0;
 
-    w1=0.9;
-    w2=5.0;
+    w1=1.2;
+    w2=10.0;
 
     //normalization
     std::vector<double> x(path.size());
