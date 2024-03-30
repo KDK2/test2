eee87a6864616705b0a851f02e2f402aeda0389d
KDK2 <kdk9678@gmail.com>
2024-03-30 22:54:21 +0900

    Successful build

--- robot.cpp
+++ robot.cpp
@@ -48,12 +48,10 @@ Robot::Robot():
     con.getGoal(cgoal,true);
 
     sen=new Sensor(is);
-    sen->addLObs(1.79,2.72,2.66,2.74);
-    sen->addLObs(1.8,2.52,1.79,2.72);
+    sen->addLObs(1.8,2.52,1.79,2.57);
     sen->addLObs(1.8,2.52,2.5,2.5);
     sen->addLObs(2.5,2.5,2.48,1.8);
-    sen->addLObs(2.48,1.8,2.68,1.81);
-    sen->addLObs(2.68,1.81,2.66,2.74);
+    sen->addLObs(2.48,1.8,2.53,1.81);
 
     // sen->addCObs(0.0,3.0,0.4);
     // sen->addCObs(-1.0,3.0,0.4);
