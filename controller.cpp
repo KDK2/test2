779634d42578eea77fd62143847f9fd668c45e56
KDK2 <kdk9678@gmail.com>
2024-03-30 21:46:39 +0900

    Successful build

--- controller.cpp
+++ controller.cpp
@@ -59,7 +59,7 @@ void Controller::setGenerator(Generator *generator)
 
 void Controller::setTemporaryGoal(double x, double y, double theta, double d)
 {
-    if(!(d>temporary.d)) return;
+    //if(!(d>temporary.d)) return;
     temporary.x=x;
     temporary.y=y;
     temporary.theta=theta;
