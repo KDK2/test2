77e82062c0624c28d9165b6783e2baa7ca6a3718
KDK <kdk9678@gmail.com>
2024-03-31 18:27:02 +0900

    Successful build

--- controller.cpp
+++ controller.cpp
@@ -108,7 +108,7 @@ void Controller::checkMaxVelocity(double vel, double vel_max, double &dst)
 #include <algorithm>
 void Controller::optimize(const double *pos, double *dst, double* cst1, double* cst2, double& loss)
 {
-    double delta=0.003;
+    double delta=0.01;
 
     std::vector<Generator::path> pfPath;
     std::vector<Generator::path> paPath;
